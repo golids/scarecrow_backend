1c08664324002040c0b862952a5c6e9382512247
golids <rebucas.goldaelove08@gmail.com>
2026-05-02 20:35:02 +0800

    AppDev: DRF backend for system implemented

--- scarecrow_backend/settings.py
+++ scarecrow_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # Third-party
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
 
 ROOT_URLCONF = 'scarecrow_backend.urls'
 
